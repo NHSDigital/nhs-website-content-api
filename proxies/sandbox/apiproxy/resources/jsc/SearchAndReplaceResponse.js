bfbab41a915b34b1d4d7ff4deba6ae1ac070cc70
Adrian Parr <adrian.parr1@nhs.net>
2023-12-07 14:07:51 +0000

    Search and replace in response policy (#48)
    * Added ‘SearchAndReplace’ Apigee policy
    
    * Renamed to ‘SearchAndReplaceResponse’
    
    * Fixed typo
    
    * Removed any ES6 syntax
    
    * Added SearchAndReplaceResponse Apigee policy to sandbox
    
    * Added JS file
    
    * Updated name
    
    * Moved flow in to the TargetEndpoint
    
    * Updated the search and replace JS
    
    * Debugging basePath and subdomain
    
    * Debug context
    
    * Debugging proxyRequest and targetRequest
    
    * WIP
    
    * Added timestamp
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Using target.name in API host name
    
    * Removed switch statement
    
    * Using a request.url
    
    * Removed targetName
    
    * Updated JS

---
+++ proxies/sandbox/apiproxy/resources/jsc/SearchAndReplaceResponse.js
@@ -0,0 +1,56 @@
+// An example requestUrl would be https://internal-dev-sandbox.apis.ptl.api.platform.nhs.uk/nwca-48/conditions/
+var requestUrl = context.getVariable("request.url");
+var environmentSubdomain = requestUrl.split("/");
+environmentSubdomain = environmentSubdomain[2];
+environmentSubdomain = environmentSubdomain.split(".");
+environmentSubdomain = environmentSubdomain[0];
+
+var apiVersion2Host;
+if (environmentSubdomain === "apis" || environmentSubdomain === "prod") {
+  apiVersion2Host = "api.service.nhs.uk";
+} else {
+  apiVersion2Host = environmentSubdomain + ".api.service.nhs.uk";
+}
+
+var searchAndReplaceStrings = [
+  {
+    searchFor: "www.nhs.uk/conditions/",
+    replaceWith: apiVersion2Host + "/nhs-website-content/conditions/",
+  },
+  {
+    searchFor: "www.nhs.uk/live-well/",
+    replaceWith: apiVersion2Host + "/nhs-website-content/live-well/",
+  },
+  {
+    searchFor: "www.nhs.uk/medicines/",
+    replaceWith: apiVersion2Host + "/nhs-website-content/medicines/",
+  },
+  {
+    searchFor: "www.nhs.uk/nhs-services/",
+    replaceWith: apiVersion2Host + "/nhs-website-content/nhs-services/",
+  },
+  {
+    searchFor: "www.nhs.uk/pregnancy/",
+    replaceWith: apiVersion2Host + "/nhs-website-content/pregnancy/",
+  },
+  {
+    searchFor: "www.nhs.uk/common-health-questions/",
+    replaceWith:
+      apiVersion2Host + "/nhs-website-content/common-health-questions/",
+  },
+  {
+    searchFor: "www.nhs.uk/mental-health/",
+    replaceWith:
+      apiVersion2Host + "api.service.nhs.uk/nhs-website-content/mental-health/",
+  },
+];
+
+var responseContent = context.getVariable("response.content");
+var regex;
+for (var i = 0; i < searchAndReplaceStrings.length; i++) {
+  var item = searchAndReplaceStrings[i];
+  regex = new RegExp(item.searchFor, "g");
+  responseContent = responseContent.replace(regex, item.replaceWith);
+}
+
+context.setVariable("response.content", responseContent);
