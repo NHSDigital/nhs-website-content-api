62e35ab5eea23ab5b9f546b22c55ea39af0b56ff
Adrian Parr <adrian.parr1@nhs.net>
2024-01-19 07:39:08 +0000

    Check if “url” property is at the root or inside the “about” object (#86)

--- proxies/live/apiproxy/resources/jsc/SearchAndReplaceResponse.js
+++ proxies/live/apiproxy/resources/jsc/SearchAndReplaceResponse.js
@@ -224,10 +224,17 @@ for (var i = 0; i < searchAndReplaceStrings.length; i++) {
 }
 
 var responseJsonObj = JSON.parse(responseContent);
-responseJsonObj.webpage = responseJsonObj.url.replace(
-  "https://" + requestHostnameEnv + "/nhs-website-content/",
-  "https://www.nhs.uk/"
-);
+if (responseJsonObj.url) {
+  responseJsonObj.webpage = responseJsonObj.url.replace(
+    "https://" + requestHostnameEnv + "/nhs-website-content/",
+    "https://www.nhs.uk/"
+  );
+} else if (responseJsonObj.about.url) {
+  responseJsonObj.about.webpage = responseJsonObj.about.url.replace(
+    "https://" + requestHostnameEnv + "/nhs-website-content/",
+    "https://www.nhs.uk/"
+  );
+}
 responseContent = JSON.stringify(responseJsonObj);
 
 context.setVariable("response.content", responseContent);
